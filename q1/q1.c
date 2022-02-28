5c04d3d2084e5f07858b0646648d0868fd93c8e6
David Fung <davidfung998@gmail.com>
2022-02-28 11:15:21 -0500

    added comments

--- q1/q1.c
+++ q1/q1.c
@@ -4,20 +4,27 @@
 #include <unistd.h>
 #include <pthread.h>
 
+// Hello world function
 void hello_world(){
     printf("hello world");
 }
 
+
+// Goodbye function
 void goodbye(){
     printf("goodbye");
 }
 
 int main(){
+
+    // Initialize threads
     pthread_t thread_id;
 
+    // Create 2 threads
     pthread_create(&thread_id, NULL, (void *) hello_world, NULL);
     pthread_create(&thread_id, NULL, (void *) goodbye, NULL);
 
+    // Exit thread
     pthread_exit(NULL);
 
     return 0;
